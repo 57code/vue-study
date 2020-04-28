0a744015df8e9930173e3d9ccb013aec69a75dab
yt0379 <yt0379@qq.com>
2020-04-28 14:50:32 +0800

    范例移到HelloWorld中

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,16 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- <communication></communication> -->
-    <!-- <slot-example></slot-example> -->
-    <!-- <form-example></form-example> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import communication from '@/components/communication';
-import SlotExample from '@/components/slots'
-import FormExample from '@/components/form'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
-    communication,
-    SlotExample,
-    FormExample
+    HelloWorld
   }
 }
 </script>
