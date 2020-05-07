b4c983a75e6711e2060e82052e387d3b2aba8eb3
yt0379 <yt0379@qq.com>
2020-05-07 22:38:59 +0800

    编译

--- src/kvue.js
+++ src/kvue.js
@@ -11,10 +11,15 @@ function defineReactive(obj, key, val) {
   // val可能还是对象，此时我们需要递归
   observe(val)
 
+  // 创建Dep实例，他和key一对一对应关系
+  const dep = new Dep()
+
   // 参数3是描述对象
   Object.defineProperty(obj, key, {
     get() {
-      console.log('get', key);
+      // console.log('get', key);
+      // 依赖收集:Dep.target就是当前新创建Watcher实例
+      Dep.target && dep.addDep(Dep.target)
       return val
     },
     set(newVal) {
@@ -23,6 +28,9 @@ function defineReactive(obj, key, val) {
         // 防止newVal是对象，提前做一次observe
         observe(newVal)
         val = newVal
+
+        // 通知更新
+        dep.notify()
       }
     }
   })
@@ -35,7 +43,7 @@ function observe(obj) {
 
   // 响应式
   new Observer(obj)
-  
+
 }
 
 // Observer: 辨别当前对象类型是纯对象还是数组，从而做不同响应式操作
@@ -108,19 +116,20 @@ class Compile {
       // 判断类型
       if (node.nodeType === 1) {
         // 元素节点
-        console.log('编译元素', node.nodeName);
-        
-      } else if(this.isInter(node)) {
+        // console.log('编译元素', node.nodeName);
+        this.compileElement(node)
+      } else if (this.isInter(node)) {
         // 文本节点
-        console.log('文本节点', node.textContent);
-        
+        // console.log('文本节点', node.textContent);
+        // double kill
+        this.compileText(node)
       }
 
       // 递归子节点
-      if(node.childNodes) {
+      if (node.childNodes) {
         this.compile(node)
       }
-    }) 
+    })
   }
 
   // 是否插值绑定
@@ -128,5 +137,110 @@ class Compile {
     return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent)
   }
 
+  // 绑定表达式解析
+  compileText(node) {
+    // 获取匹配表达式 RegExp.$1,比如counter， vm['counter']
+    // node.textContent = this.$vm[RegExp.$1]
+    this.update(node, RegExp.$1, 'text')
+  }
 
+  // 编译元素节点：判断指令和事件
+  compileElement(node) {
+    // 获取属性
+    const attrs = node.attributes
+
+    Array.from(attrs).forEach(attr => {
+      // k-text="counter"
+      // attr是一个对象{name:'k-text', value: 'counter'}
+      const { name, value } = attr
+      // 判断是否是指令
+      if (name.indexOf('k-') === 0) {
+        // 截取指令
+        const dir = name.substring(2)
+        // 指令指令
+        this[dir] && this[dir](node, value)
+      }
+      // 判断是否是事件 @
+      // else if() {
+
+      // }
+    })
+  }
+
+  // k-text文本更新
+  text(node, exp) {
+    this.update(node, exp, 'text')
+  }
+
+  // k-html
+  html(node, exp) {
+    this.update(node, exp, 'html')
+  }
+
+  // update方法，高阶函数：除了执行dom操作，创建一个额外watcher实例
+  // dir是指令名称
+  update(node, exp, dir) {
+    // 获取更新方法
+    const fn = this[dir + 'Updater']
+    // 初始化，让用户看到首次渲染结果
+    fn && fn(node, this.$vm[exp])
+
+    // 创建watcher实例
+    new Watcher(this.$vm, exp, val => {
+      fn && fn(node, val)
+    })
+  }
+
+  // dom执行方法
+  textUpdater(node, value) {
+    node.textContent = value
+  }
+
+  htmlUpdater(node, value) {
+    node.innerHTML = value
+  }
+}
+
+// Watcher: 管理依赖，执行更新
+// const watchers = []
+class Watcher {
+  // vm是KVue实例
+  // key是data中对应的key名称
+  // fn是更新函数，他知道怎么更新dom
+  constructor(vm, key, fn) {
+    this.vm = vm
+    this.key = key
+    this.fn = fn
+
+    // watchers.push(this)
+
+    // 建立dep和watcher之间的关系
+    Dep.target = this
+    this.vm[this.key] // 读一下key的值触发其getter
+    Dep.target = null
+  }
+
+  // 更新函数，由Dep调用
+  update() {
+    // 更新函数调用，设置上下文问KVue实例，传参是当前最新值
+    this.fn.call(this.vm, this.vm[this.key])
+  }
+
+}
+
+// Dep: 管理多个watcher实例，当对应key发生变化时，通知他们更新
+class Dep {
+  constructor() {
+    this.deps = []
+  }
+
+  addDep(dep) {
+    // 添加订阅者，dep就是watcher实例
+    this.deps.push(dep)
+  }
+
+  // 通知更新
+  notify() {
+    this.deps.forEach(w => w.update())
+  }
 }
