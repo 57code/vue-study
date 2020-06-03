e598aeefd8eead048f3640c51a382a24c72daf72
yt0379 <yt0379@qq.com>
2020-06-03 22:32:05 +0800

    编译

--- kvue.js
+++ kvue.js
@@ -3,17 +3,28 @@ function defineReactive(obj, key, val) {
 
   // 递归处理
   observe(val)
-  
+
+  // 创建一个Dep实例
+  const dep = new Dep()
+
   Object.defineProperty(obj, key, {
-    get(){
+    get() {
       console.log('get', key);
+
+      // 依赖收集: 把watcher和dep关联
+      // 希望Watcher实例化时，访问一下对应key，同时把这个实例设置到Dep.target上面
+      Dep.target && dep.addDep(Dep.target)
+
       return val
     },
-    set(newVal){
+    set(newVal) {
       if (newVal !== val) {
         console.log('set', key, newVal);
         observe(newVal)
         val = newVal
+
+        // 通知更新
+        dep.notify()
       }
     },
   })
@@ -21,7 +32,7 @@ function defineReactive(obj, key, val) {
 
 // 让我们使一个对象所有属性都被拦截
 function observe(obj) {
-  if(typeof obj !== 'object' || obj == null) {
+  if (typeof obj !== 'object' || obj == null) {
     return
   }
 
@@ -57,6 +68,9 @@ class KVue {
 
     // 代理
     proxy(this)
+
+    // 编译器
+    new Compiler('#app', this)
   }
 }
 
@@ -76,8 +90,132 @@ class Observer {
 }
 
 // Compiler: 解析模板，找到依赖，并和前面拦截的属性关联起来
+// new Compiler('#app', vm)
 class Compiler {
+  constructor(el, vm) {
+    this.$vm = vm
+    this.$el = document.querySelector(el)
+
+    // 执行编译
+    this.compile(this.$el)
+  }
+
+  compile(el) {
+    // 遍历这个el
+    el.childNodes.forEach(node => {
+      // 是否是元素
+      if (node.nodeType === 1) {
+        // console.log('编译元素', node.nodeName)
+        this.compileElement(node)
+      } else if (this.isInter(node)) {
+        // console.log('编译文本', node.textContent);
+        this.compileText(node)
+      }
+
+      // 递归
+      if (node.childNodes) {
+        this.compile(node)
+      }
+    })
+
+  }
+
+  // 解析绑定表达式{{}}
+  compileText(node) {
+    // 获取正则匹配表达式，从vm里面拿出它的值
+    // node.textContent = this.$vm[RegExp.$1]
+    this.update(node, RegExp.$1, 'text')
+  }
+
+  // 编译元素
+  compileElement(node) {
+    // 处理元素上面的属性，典型的是k-，@开头的
+    const attrs = node.attributes
+    Array.from(attrs).forEach(attr => {
+      // attr:   {name: 'k-text', value: 'counter'}
+      const attrName = attr.name
+      const exp = attr.value
+      if (attrName.indexOf('k-') === 0) {
+        // 截取指令名称 text
+        const dir = attrName.substring(2)
+        // 看看是否存在对应方法，有则执行
+        this[dir] && this[dir](node, exp)
+      }
+      // 事件处理
+    })
+  }
+
+  // k-text
+  text(node, exp) {
+    // node.textContent = this.$vm[exp]
+    this.update(node, exp, 'text')
+  }
+
+  // k-html
+  html(node, exp) {
+    // node.innerHTML = this.$vm[exp]
+    this.update(node, exp, 'html')
+  }
+
+  // dir:要做的指令名称
+  update(node, exp, dir) {
+    // 初始化
+    const fn = this[dir + 'Updater']
+    fn && fn(node, this.$vm[exp])
+
+    // 更新，创建一个Watcher实例
+    new Watcher(this.$vm, exp, val => {
+      fn && fn(node, val)
+    })
+
+  }
+
+  textUpdater(node, val) {
+    node.textContent = val
+  }
+
+  htmlUpdater(node, val) {
+    node.innerHTML = val
+  }
+
+  // 文本节点且形如{{xx}}
+  isInter(node) {
+    return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent)
+  }
+
+}
+
+// 管理一个依赖，未来执行更新
+class Watcher {
+  constructor(vm, key, updateFn) {
+    this.vm = vm
+    this.key = key
+    this.updateFn = updateFn
+
+    // 读一下当前key，触发依赖收集
+    Dep.target = this
+    vm[key]
+    Dep.target = null
+  }
+
+  // 未来会被dep调用
+  update() {
+    this.updateFn.call(this.vm, this.vm[this.key])
+  }
+}
+
+// Dep: 保存所有watcher实例，当某个key发生变化，通知他们执行更新
+class Dep { 
+
   constructor() {
-    
+    this.deps = []
+  }
+
+  addDep(watcher) {
+    this.deps.push(watcher)
+  }
+  
+  notify() {
+    this.deps.forEach(dep => dep.update())
   }
 }
