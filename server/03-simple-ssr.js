3943694f32c31ba1119ef4cd19c042909cc2a4fc
yt0379 <yt0379@qq.com>
2020-06-12 20:55:41 +0800

    ff

---
+++ server/03-simple-ssr.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+// 导入vue
+const Vue = require('vue')
+// 渲染器
+const { createRenderer } = require('vue-server-renderer')
+// 得到一个渲染器可以直接渲染vue实例
+const renderer = createRenderer()
+
+// 路由和同构
+app.get('/', async (req, res) => {
+  // 创建vue实例
+  const vm = new Vue({
+    data() {
+      return {
+        name: '村长真棒'
+      }
+    },
+    template: '<div>{{name}}</div>'
+  })
+  try {
+    // 渲染获取html字符串
+    const html = await renderer.renderToString(vm)
+
+    res.send(html)
+  } catch (error) {
+    // 500错误
+    res.status(500).send('Internal Server Error')
+  }
+
+})
+
+// 端口
+app.listen(3000)
