df6ecb7a323e53dda655b36120a4661dc3ecab26
yt0379 <yt0379@qq.com>
2020-07-14 18:29:46 +0800

    f

--- src/main.js
+++ src/main.js
@@ -1,19 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-// import router from './router'
-import createRouter from "./router";
+import router from './router'
 
 Vue.config.productionTip = false;
 
-// 每个请求一个实例
-// 调用者是entry-server
-export function createApp(context) {
-  const router = createRouter()
-  const app = new Vue({
-    router,
-    context,
-    render: h => h(App)
-  }).$mount("#app");
-  return {app, router}
-}
-
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
