1227e97f940ad46db59ae2123fb71cd799b89e21
yt0379 <yt0379@qq.com>
2020-07-14 18:36:45 +0800

    init

--- server/02-csr.js
+++ server/02-csr.js
@@ -3,17 +3,13 @@
 const express = require('express')
 const app = express()
 
-// 静态文件服务
-const publicPath = require('path').resolve(__dirname, '../public')
-app.use(express.static(publicPath, {index: false}))
-
 app.get('/', function (req, res) {
   const html = `
     <div id="app">
       <h1>{{title}}</h1>
       <p>{{content}}</p>
     </div>
-    <script src="vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script>
       new Vue({
         el:'#app',
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <HelloWorld></HelloWorld>
+
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view></router-view>
   </div>
 </template>
 
