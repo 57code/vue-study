0a14245645b2a0c4065e699f06a493bfbd78d6dd
yt0379 <yt0379@qq.com>
2020-08-17 20:08:32 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
-import router from './krouter'
-import store from './kstore'
-// import store from './store'
-// import router from './router'
+// import router from './krouter'
+// import store from './kstore'
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 // 事件总线
@@ -13,7 +13,6 @@ Vue.prototype.$bus = new Vue()
 new Vue({
   // 挂载，目的是什么？让我们可以在插件中访问到Router实例
   router,
-
   store,
   render: h => h(App)
 }).$mount('#app')
