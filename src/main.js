72c128d6b3d87e29da943c92efbba2e4aee6ac3d
yt0379 <yt0379@qq.com>
2020-09-01 20:19:58 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import router from './router'
 
 Vue.config.productionTip = false
 // 事件总线
 Vue.prototype.$bus = new Vue()
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
