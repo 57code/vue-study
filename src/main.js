94b5a31faba0170c44a701e8d82f1ef59a9403ad
yt0379 <yt0379@qq.com>
2020-09-01 20:20:56 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 // 事件总线
@@ -9,5 +10,6 @@ Vue.prototype.$bus = new Vue()
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
