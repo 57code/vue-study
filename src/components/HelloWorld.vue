27ead45a9b0540acaddbe59d2d7030a439f561d7
yt0379 <yt0379@qq.com>
2020-09-15 11:53:31 +0800

    起始点

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,57 @@
 <template>
-  <div>
-    <!-- store是哪来的，state是响应式的 -->
-    <!-- Store类得有commit方法可以执行mutation -->
-    <p @click="$store.commit('add')">{{$store.state.count}}</p>
-    <p @click="$store.dispatch('asyncAdd')">async:{{$store.state.count}}</p>
-    <!-- <p>{{$store.getters.doubleCount}}</p> -->
-
-    <p>{{xx}}</p>
-    <button @click="sayHi">say hi</button>
-    <!-- $attrs -->
-    <p v-on="$listeners">{{$attrs.msg}}</p>
-    <!-- provide/inject -->
-    <p>{{bar2}}</p>
-    <p>{{app.$options.name}}</p>
-    <!-- 插槽 -->
-    <slot></slot>
-    <div>
-      <slot name="content" baz="content from child"></slot>
-    </div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
   </div>
 </template>
 
-<script>
-  export default {
-    inject: {
-      bar2: 'bar',
-      app: 'app'
-    },
-    data() {
-      return {
-        xx: 'xx',
-        bar: 'my bar'
-      }
-    },
-    mounted() {
-      console.log(this.$store);
-      
-      setTimeout(() => {
-        // this.$store.state = {}
-        this.$store.state.count++
-      }, 1000);
-      
-      // 监听事件
-      this.$root.$on('foo', msg => {
-        console.log(msg);
-        
-      })
-    },
-    methods: {
-      sayHi() {
-        // 派发者和监听者是同一个
-        // this.$parent.$emit('foo', 'something from brother')
-        this.$root.$emit('foo', 'something from brother')
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-      }
-    },
-  }
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
