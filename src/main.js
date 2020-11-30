4842b2fe7c740b4f112b5c40b09754e1a3cd5178
57code <yt0379@sohu.com>
2020-11-30 21:31:18 +0800

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
-import router from './router'
+// import router from './router'
+import router from './krouter'
 
 Vue.config.productionTip = false
 // 事件总线
 Vue.prototype.$bus = new Vue()
 
+// this.$router.push()
+// Vue.prototype.$router = router
 new Vue({
   router,
   render: h => h(App)
