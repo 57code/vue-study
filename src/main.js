63151860c98e8954a2b13dcdb8cd3a1c14a271ae
57code <yt0379@sohu.com>
2021-06-29 19:33:27 +0800

    add router vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 // 事件总线
 Vue.prototype.$bus = new Vue()
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
