f1535dc32c160dc85052a5d203c2cef02cd1038a
57code <yt0379@sohu.com>
2021-07-01 21:35:55 +0800

    编译文本

--- kvue/kvue.js
+++ kvue/kvue.js
@@ -53,5 +53,48 @@ class KVue {
     proxy(this)
     
     // 3.编译
+    new Compile(options.el, this)
+  }
+}
+
+class Compile {
+  constructor(el, vm) {
+    // 保存KVue实例
+    this.$vm = vm
+
+    // 编译模板树
+    this.compile(document.querySelector(el))
+  }
+
+  // el模板根节点
+  compile(el) {
+    // 遍历el
+    // 1.获取el所有子节点
+    el.childNodes.forEach(node => {
+      // 2.判断node类型
+      if (node.nodeType === 1) {
+        // 元素
+        console.log('element', node.nodeName);
+
+        // 递归
+        if (node.childNodes.length > 0) {
+          this.compile(node)
+        }
+      } else if (this.isInter(node)) {
+        // 插值文本
+        console.log('text', node.textContent);
+        this.compileText(node)
+      }
+    })
+  }
+
+  // 处理插值文本
+  compileText(node) {
+    node.textContent = this.$vm[RegExp.$1]
+  }
+  
+  // {{ooxx}}
+  isInter(node) {
+    return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent)
   }
 }
